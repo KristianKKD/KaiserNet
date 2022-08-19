705e3f1d565fe3985a683ab8dbbb2f2b0a82fdc9
KristianKKD <kkddimitrov@gmail.com>
2022-08-19 17:45:50 +0100

    2, made the output step

--- KaiserNet/Layer.hpp
+++ KaiserNet/Layer.hpp
@@ -1,45 +1,73 @@
 #include "Shared.hpp"
 
-#define MAXSIZE 1024
+#define MAXSIZE 128
 
 enum LayerType {
 	Input,
-	Hidden,
 	Output,
+	Hidden,
 };
 
 class Node {
 public:
 	double bias;
 	double heldValue;
-	double incomingWeights[MAXSIZE];
+	double outgoingWeights[MAXSIZE];
 
 	Node(LayerType myType) {
 		bias = InitializeBias(myType);
 		heldValue = bias;
+		for (int i = 0; i < MAXSIZE; i++)
+			outgoingWeights[i] = bias;
 	}
+
+	~Node() {}
 
 	double InitializeBias(LayerType myType) { //can't be zero
-		return 0.5 * (!(myType == Input || myType == Output));
+		return 0.01;
 	}
 
-	double ActivationFunction(double sum) {
+	void Pulse(double incomingValue) {
+
+	}
+
+	double GetWeightTowards(int nodeIndex) {
+		return outgoingWeights[nodeIndex];
+	}
+
+	void Activate(double sum) {
+		sum += bias;
 		if (sum > 1)
 			sum = 1;
 		else if (sum < 0)
 			sum = 0;
-		return sum;
+		
+		heldValue = sum;
 	}
 };
 
 class NodeLayer {
 public:
 	LayerType myType;
-	Node internalNodes[];
+	vector<Node> nodes;
+
+	NodeLayer(vector<double> target) { //a node layer created with a target will always be an input layer
+		vector<Node> newNodes;
+		for (int i = 0; i < target.size(); i++)
+			newNodes.push_back(Node(Input));
+
+		nodes = newNodes;
+		myType = Input;
+	}
 
 	NodeLayer(int layerSize, LayerType lType) {
-		myType = lType;
+		vector<Node> newNodes;
 		for (int i = 0; i < layerSize; i++)
-			internalNodes[i] = Node(myType);
+			newNodes.push_back(Node(lType));
+
+		nodes = newNodes;
+		myType = lType;
 	}
+
+	~NodeLayer() {}
 };
--- KaiserNet/Main.cpp
+++ KaiserNet/Main.cpp
@@ -1,8 +1,90 @@
-#include "Shared.hpp"
+#include "Layer.hpp"
 
-void read_mnist(string path);
+//HYPER PARAMETERS
+#define LEARNINGRATE = 0.1;
+
+//void read_mnist(string path);
+void GenerateOutput(int stepCount);
+void ReadInput();
+void Learn();
+
+vector<NodeLayer> layerList;
+string input;
+
+bool pause;
+vector<double> target;
 
 int main() {
-	Log("KaiserNet version: 0.0.0");
-	read_mnist("C:\\Users\\KrabGor\\Downloads\\labels\\t10k-images.idx3-ubyte");
+	Log("KaiserNet version: 0.0.2");
+	//read_mnist("C:\\Users\\KrabGor\\Downloads\\labels\\t10k-images.idx3-ubyte");
+
+	int inputLayerSize = 2;
+	int outputLayerSize = 2;
+
+	int hiddenLayerCount = 1;
+
+	target = { 0.2, 0.9 };
+	NodeLayer inputLayer = NodeLayer(target); //input layer
+	layerList.push_back(inputLayer);
+
+	for (int i = 0; i < hiddenLayerCount; i++)
+		layerList.push_back(NodeLayer(hiddenLayerCount, Hidden)); //generate the hidden layers
+
+	NodeLayer outputLayer = NodeLayer(outputLayerSize, Output); //output layer
+	layerList.push_back(outputLayer);
+
+	thread inputThread = thread(ReadInput);
+
+	for (int i = 0; input != "stop"; i++) {
+		while (pause) {}
+		GenerateOutput(i);
+		Learn();
+	}
+
+	inputThread.join();
+	inputThread.~thread();
+}
+
+void ReadInput() {
+	while (input != "stop") {
+		input = "";
+		std::cin >> input;
+		if (input == "pause")
+			pause = !pause;
+	}
+}
+
+void GenerateOutput(int stepCount) {
+	for (int layerIndex = 1; layerIndex < layerList.size(); layerIndex++) { //go through all the layers, skipping input layer (as that will be accounted for)
+		NodeLayer currentLayer = layerList[layerIndex];
+		NodeLayer prevLayer = layerList[layerIndex - 1];
+
+		for (int nodeIndex = 0; nodeIndex < currentLayer.nodes.size(); nodeIndex++) { //for each node in the current layer
+			//for each node, sum up the previous layer's nodes' values
+			double sum = 0;
+
+			for (int prevNodeIndex = 0; prevNodeIndex < prevLayer.nodes.size(); prevNodeIndex++)
+				sum += prevLayer.nodes[prevNodeIndex].heldValue +
+				prevLayer.nodes[prevNodeIndex].GetWeightTowards(nodeIndex);
+
+			currentLayer.nodes[nodeIndex].Activate(sum); //make the node's value non-linear + bias
+		}
+
+		if (layerIndex == layerList.size() - 1 && stepCount % 10000 == 0) {
+			string outputString;
+			for (int nodeIndex = 0; nodeIndex < currentLayer.nodes.size(); nodeIndex++) { //for each node in the output layer
+				outputString += ((nodeIndex != 0) ? " ": "") + to_string(nodeIndex) + ": " + to_string(currentLayer.nodes[nodeIndex].heldValue);
+			}
+
+			Log(outputString);
+		}
+	}
+}
+
+void Learn() {
+	for (int layerIndex = 1; layerIndex < layerList.size(); layerIndex++) { //go through all the layers, skipping input layer (as that will be accounted for)
+		for (int nodeIndex = 0; nodeIndex < layerList[layerIndex].nodes.size(); nodeIndex++) { //for each node in the current layer
+			//RandomLearn();
+		}
+	}
 }
